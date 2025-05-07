e6ffec68c2b8c685cffbb4f16a1e78a05d86a6c3
Clebson1094 <clebson167.cj@gmail.com>
2025-05-07 12:56:12 -0300

    Update el-doria.py
    Alterado o nome tela, corrigido o erro no código, adicionado mais linhas

--- el-doria.py
+++ el-doria.py
@@ -3,8 +3,28 @@ import os
 
 pygame.init()
 
-#Posição da tela
+#TELA
 largura = 800
 altura = 600
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("El-Doria)
+pygame.display.set_caption("El-Doria")
+fonte = pygame.font.Sysfont(None, 60)
+texto = fonte.render("titulo", True, (255, 255, 255))
+titulo = "El-Doria"
+meio_da_tela = (150, 250)
+preto = (0, 0, 0)
+
+rodando = True
+while rodando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+
+
+
+    
+
+    tela.fill((preto))
+    tela.blit(texto (meio_da_tela))
+    pygame.display.update()
+pygame.quit()
