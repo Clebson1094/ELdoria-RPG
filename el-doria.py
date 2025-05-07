b5af9eb7036bf801307875cf9d0ff4bae5481fc8
Clebson1094 <clebson167.cj@gmail.com>
2025-05-07 14:03:32 -0300

    Corrigido
    Corrigido erros e adicionado pulos além de por quadrado vermelho para visualização

--- el-doria.py
+++ el-doria.py
@@ -12,17 +12,67 @@ fonte = pygame.font.SysFont(None, 60)
 texto = fonte.render("El-Doria", True, (255, 255, 255))
 preto = (0, 0, 0)
 
+#CLOCK
+clock = pygame.time.Clock()
+
+#MOVIMENTAÇÃO DO PERSONAGEM
+x = 300
+y = 300
+velocidade = 4
+
+pulo = False
+forca_pulo = 25
+gravidade = 1
+vel_y = 5
+
+#CONTROLES
+def controles(teclas):
+    global x, y, pulo, vel_y
+
+    if teclas[pygame.K_LEFT]:
+        x -= velocidade
+    if teclas [pygame.K_RIGHT]:
+        x += velocidade
+
+    if not pulo:
+        if teclas[pygame.K_SPACE]:
+            pulo = True
+            vel_y = -forca_pulo
+    else:
+        y += vel_y
+        vel_y += gravidade
+
+        #ENCOSTANDO NO CHÃO
+        if y >= 300:
+            y = 300
+            pulo = False
+
 rodando = True
 while rodando:
+    clock.tick(60)
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
-
-
-
     
+    teclas = pygame.key.get_pressed()
+    controles(teclas)
+
+    if pulo:
+        y += vel_y
+        vel_y += gravidade
+        if y >= 300:
+            y = 300
+            vel_y = 0
+            pulo = False
 
     tela.fill((preto))
+    
+    personagem = pygame.Rect(x, y, 50, 50)
+    pygame.draw.rect(tela, (255, 0, 0), personagem)
+    
+    
+
     tela.blit(texto, (326, 260))
     pygame.display.update()
+
 pygame.quit()
