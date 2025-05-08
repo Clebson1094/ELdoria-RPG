0c08f1e2e886c29ed21de569d108145aa6df59c2
Clebson1094 <clebson167.cj@gmail.com>
2025-05-08 10:24:43 -0300

    Adicionado pulo
    Ainda contem erros no pulo, ela está pulando apenas com animação para um lado

--- el-doria.py
+++ el-doria.py
@@ -12,11 +12,32 @@ fonte = pygame.font.SysFont(None, 60)
 texto = fonte.render("", True, (255, 255, 255))
 preto = (0, 0, 0)
 
+#SPRITESHEET_PULO
+caminho_pulo = os.path.join("champ", "Jump_KG_1.png")
+spritesheet_jump = pygame.image.load(caminho_pulo).convert_alpha()
+largura_pulo = 100
+altura_pulo = 64
+quantidade_jump_frames = 6
+
+jump_frames = []
+for i in range(quantidade_jump_frames):
+    frame = spritesheet_jump.subsurface((i * largura_pulo, 0, largura_pulo, altura_pulo))
+    jump_frames.append(frame)
+
+pulo = False
+velocidade_y = 0
+forca_pulo = 15
+gravidade = 1
+y_chao = 300
+indice_pulo = 0
+tempo_pulo = 100
+ultimo_pulo_update = pygame.time.get_ticks()
+
 #SPRITESHEET_WALKING
 caminho_walk = os.path.join("champ", "walk.png.png")
 spritesheet_walk = pygame.image.load(caminho_walk).convert_alpha()
 
-largura_walk = 64
+largura_walk = 100
 altura_walk = 64
 quantidade_walk_frames = 7
 
@@ -29,15 +50,14 @@ walk_frames_esquerda = [pygame.transform.flip(f, True, False) for f in walk_fram
 
 x = 300
 y = 300
-velocidade = 10
+velocidade = 4
 direcao = "idle"
 
-#spritessheet
+#SPRITES_IDLE
 caminho_idle = os.path.join("champ", "idle.png.png")
 spritesheet_idle = pygame.image.load(caminho_idle).convert_alpha()
 
-#CONFIG DO SPRITESHEET
-largura_frame = 64
+largura_frame = 100
 altura_frame = 64
 quantidade_frames = 4
 idle_frames = []
@@ -46,8 +66,6 @@ for i in range(quantidade_frames):
     frame = spritesheet_idle.subsurface((i * largura_frame, 0, largura_frame, altura_frame))
     idle_frames.append(frame)
 
-
-#CONTROLE DE ANIMAÇÃO
 indice_frame = 0
 tempo_animacao = 180
 ultimo_update = pygame.time.get_ticks()
@@ -61,7 +79,24 @@ while rodando:
     clock.tick(60)
 
     teclas = pygame.key.get_pressed()
+    if not pulo:
+        if teclas[pygame.K_UP]:
+            pulo = True
+            velocidade_y = -forca_pulo
+            indice_pulo = 0
+    else:
+        y += velocidade_y
+        velocidade_y += gravidade
 
+        agora = pygame.time.get_ticks()
+        if agora - ultimo_pulo_update > tempo_pulo:
+            indice_pulo = (indice_pulo + 1) % len(jump_frames)
+            ultimo_pulo_update = agora
+        
+        if y >= y_chao:
+            y = y_chao
+            pulo = False
+    
     if teclas[pygame.K_LEFT]:
         x -= velocidade
         direcao = "esquerda"
@@ -79,11 +114,11 @@ while rodando:
             indice_frame = (indice_frame + 1) % quantidade_walk_frames
         ultimo_update = agora
 
-
-    
     tela.fill((preto))
     
-    if direcao == "idle":
+    if pulo:
+        tela.blit(jump_frames[indice_pulo], (x, y))
+    elif direcao == "idle":
         tela.blit(idle_frames[indice_frame % len(idle_frames)], (x, y))
     elif direcao == "direita":
         tela.blit(walk_frames_direita[indice_frame % len(walk_frames_direita)], (x, y))
