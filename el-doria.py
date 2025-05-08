5b8e8bbf85778c3d57e9786e6edb7c5721f04701
Clebson1094 <clebson167.cj@gmail.com>
2025-05-07 23:15:20 -0300

    Adicionado personagem
    Adicionado personagem mas contem erros, amanhã tento resolver

--- el-doria.py
+++ el-doria.py
@@ -2,71 +2,94 @@ import pygame
 import os
 
 pygame.init()
-
+clock = pygame.time.Clock()
 #TELA
-largura = 800
-altura = 600
+largura = 1000
+altura = 800
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("El-Doria")
 fonte = pygame.font.SysFont(None, 60)
-texto = fonte.render("El-Doria", True, (255, 255, 255))
+texto = fonte.render("", True, (255, 255, 255))
 preto = (0, 0, 0)
 
-#CLOCK
-clock = pygame.time.Clock()
+#SPRITESHEET_WALKING
+caminho_walk = os.path.join("champ", "walk.png.png")
+spritesheet_walk = pygame.image.load(caminho_walk).convert_alpha()
 
-#MOVIMENTAÇÃO DO PERSONAGEM
+largura_walk = 64
+altura_walk = 64
+quantidade_walk_frames = 7
+
+walk_frames_direita = []
+for i in range(quantidade_walk_frames):
+    frame = spritesheet_walk.subsurface((i * largura_walk, 0, largura_walk, altura_walk))
+    walk_frames_direita.append(frame)
+
+walk_frames_esquerda = [pygame.transform.flip(f, True, False) for f in walk_frames_direita]
+
 x = 300
 y = 300
-velocidade = 4
+velocidade = 10
+direcao = "idle"
 
-pulo = False
-forca_pulo = 25
-gravidade = 2
-vel_y = 1
+#spritessheet
+caminho_idle = os.path.join("champ", "idle.png.png")
+spritesheet_idle = pygame.image.load(caminho_idle).convert_alpha()
 
-#CONTROLES
-def controles(teclas):
-    global x, y, pulo, vel_y
+#CONFIG DO SPRITESHEET
+largura_frame = 64
+altura_frame = 64
+quantidade_frames = 4
+idle_frames = []
 
-    if teclas[pygame.K_LEFT]:
-        x -= velocidade
-    if teclas [pygame.K_RIGHT]:
-        x += velocidade
+for i in range(quantidade_frames):
+    frame = spritesheet_idle.subsurface((i * largura_frame, 0, largura_frame, altura_frame))
+    idle_frames.append(frame)
 
-    if not pulo:
-        if teclas[pygame.K_SPACE]:
-            pulo = True
-            vel_y = -forca_pulo
-    else:
-        y += vel_y
-        vel_y += gravidade
 
-        #ENCOSTANDO NO CHÃO
-        if y >= 300:
-            y = 300
-            pulo = False
+#CONTROLE DE ANIMAÇÃO
+indice_frame = 0
+tempo_animacao = 180
+ultimo_update = pygame.time.get_ticks()
+
 
 rodando = True
 while rodando:
-    clock.tick(60)
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
-    
+    clock.tick(60)
+
     teclas = pygame.key.get_pressed()
-    controles(teclas)
 
-   
+    if teclas[pygame.K_LEFT]:
+        x -= velocidade
+        direcao = "esquerda"
+    elif teclas[pygame.K_RIGHT]:
+        x += velocidade
+        direcao = "direita"
+    else:
+        direcao = "idle"
+    
+    agora = pygame.time.get_ticks()
+    if agora - ultimo_update > tempo_animacao:
+        if direcao == "idle":
+            indice_frame = (indice_frame + 1) % len(idle_frames)
+        else:
+            indice_frame = (indice_frame + 1) % quantidade_walk_frames
+        ultimo_update = agora
 
+
+    
     tela.fill((preto))
     
-    personagem = pygame.Rect(x, y, 50, 50)
-    pygame.draw.rect(tela, (255, 0, 0), personagem)
-    
-    
+    if direcao == "idle":
+        tela.blit(idle_frames[indice_frame % len(idle_frames)], (x, y))
+    elif direcao == "direita":
+        tela.blit(walk_frames_direita[indice_frame % len(walk_frames_direita)], (x, y))
+    elif direcao == "esquerda":
+        tela.blit(walk_frames_esquerda[indice_frame % len(walk_frames_esquerda)], (x, y))
 
-    tela.blit(texto, (326, 260))
     pygame.display.update()
 
 pygame.quit()
